4874a7f782525dcf4aff5a976d7f4dfe6edf84c7
Alice Wang <alice.y.wang15@gmail.com>
2019-03-13 19:26:44 -0700

    Add files via upload
    A simple to-do list

---
+++ todo.js
@@ -0,0 +1,35 @@
+function newItem() {
+    var item = document.getElementById("input").value;
+    var ul = document.getElementById("list");
+    var li = document.createElement("li");
+
+    ul.appendChild(li);
+    document.getElementById("input").value = "";
+    li.onclick = removeItem;
+  }
+  
+  document.body.onkeyup = function(e) {
+    if (e.keyCode == 13) {
+      newItem();
+    }
+  };
+
+  function newItem() {
+    var item = document.getElementById("input").value;
+    var ul = document.getElementById("list");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(item));
+    ul.appendChild(li);
+    document.getElementById("input").value = "";
+    li.onclick = removeItem;
+  }
+  
+  document.body.onkeyup = function(e) {
+    if (e.keyCode == 13) {
+      newItem();
+    }
+  };
+  
+  function removeItem(e) {
+    e.target.parentElement.removeChild(e.target);
+  }
